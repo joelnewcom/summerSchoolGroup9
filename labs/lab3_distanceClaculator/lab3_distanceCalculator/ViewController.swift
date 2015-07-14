2618356acfc7d50af4426d3cd4a3b21659517cd6
Joel Neukom <joelneukom@Joels-MacBook-Pro.local>
2015-07-14 15:07:15 -0400

    lab3

--- labs/lab3_distanceClaculator/lab3_distanceCalculator/ViewController.swift
+++ labs/lab3_distanceClaculator/lab3_distanceCalculator/ViewController.swift
@@ -9,19 +9,50 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var convertButton: UIButton!
+    @IBOutlet weak var clearButton: UIButton!
+    @IBOutlet weak var resultsField: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
     
-
-
+    func convertToDouble(numberAsString:String)->Double{
+        
+        var doubleNumber:Double = 0
+        if let number = NSNumberFormatter().numberFromString(numberAsString) {
+            doubleNumber = number.doubleValue
+        }
+        return doubleNumber
+    }
+    
+    @IBAction func convertToKilometers(sender: AnyObject) {
+        
+        var resultString:String
+        var doubleVal:Double = convertToDouble(self.textField.text)
+        
+        //converting into kilometers
+        doubleVal = doubleVal * 1.609344
+        resultString = "Distance in Kilometers: \(doubleVal)"
+        
+        resultsField.text = resultString
+        
+    }
+    @IBAction func clearResultAndTextfield(sender: UIButton) {
+        
+        textField.text = ""
+        resultsField.text = ""
+    }
+    
+    
 }
 
