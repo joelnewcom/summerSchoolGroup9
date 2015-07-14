40f5c81890de8d2f2d5fea8c088a9b9a77046819
Danny Selgo <selgod@mail.gvsu.edu>
2015-07-14 16:33:43 -0400

    Added name

--- labs/lab3_distanceClaculator/lab3_distanceCalculator/AppDelegate.swift
+++ labs/lab3_distanceClaculator/lab3_distanceCalculator/AppDelegate.swift
@@ -2,7 +2,7 @@
 //  AppDelegate.swift
 //  lab3_distanceCalculator
 //
-//  Created by Joel Neukom on 14/07/15.
+//  Created by Joel Neukom  & Danny Selgo on 14/07/15.
 //  Copyright (c) 2015 Joel Neukom. All rights reserved.
 //
 
