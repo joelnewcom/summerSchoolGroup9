a14b06faa2c68bdf5e8f3fa4b9a1cadd9058392c
Joel Neukom <joelneukom@Joels-MacBook-Pro.local>
2015-07-14 10:29:39 -0400

    lab3

---
+++ labs/lab3_distanceClaculator/lab3_distanceCalculator/lab3_distanceCalculatorTests/lab3_distanceCalculatorTests.swift
@@ -0,0 +1,36 @@
+//
+//  lab3_distanceCalculatorTests.swift
+//  lab3_distanceCalculatorTests
+//
+//  Created by Joel Neukom on 14/07/15.
+//  Copyright (c) 2015 Joel Neukom. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class lab3_distanceCalculatorTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
